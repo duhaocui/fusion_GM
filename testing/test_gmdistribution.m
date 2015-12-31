e388e54c66ba7f87096a8f376b87da38f14d0657
Colin Lu <landcolin@gmail.com>
2015-12-31 08:50:34 +0800

    add test_gmdistribution

---
+++ testing/test_gmdistribution.m
@@ -0,0 +1,10 @@
+clear
+clc
+close all
+
+mu = [1 2;-3 -5];
+sigma = cat(3,[2 0;0 .5],[1 0;0 1]);
+p = ones(1,2)/2;
+obj = gmdistribution(mu,sigma,p);
+
+ezsurf(@(x,y)pdf(obj,[x y]),[-10 10],[-10 10])
